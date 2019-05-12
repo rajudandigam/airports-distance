aba290ae81232da12030bd397b213c9e8c772ce9
Raju Dandigam <raju0574@gmail.com>
2019-05-12 15:48:59 -0700

    feat: added get distance logic

--- src/index.js
+++ src/index.js
@@ -69,6 +69,133 @@
 		suggestionList.innerHTML = '';
 	}
 
+	//Finds the distance between two airports and updates the google maps with two airports
+	function findAirportsDistance(event) {
+		var dist,
+			distanceContainer = distanceForm.querySelector('#DistanceContainer'),
+			fromAirportSpan = distanceContainer.querySelector('#FromAirportSpan'),
+			toAirportSpan = distanceContainer.querySelector('#ToAirportSpan'),
+			distanceSpan = distanceContainer.querySelector('#DistanceSpan');
+
+		event.preventDefault();
+
+		if(!fromAirportObj || !toAirportObj) {
+			alert("Please select airport from suggestion list");
+			return;
+		}
+
+		dist = distance(fromAirportObj.lat, fromAirportObj.lon, toAirportObj.lat, toAirportObj.lon, 'N');
+		distanceContainer.classList.remove('is-hidden');
+		fromAirportSpan.textContent = fromAirportObj.name;
+		toAirportSpan.textContent = toAirportObj.name;
+		distanceSpan.textContent = Math.round(dist).toFixed(2);
+
+		updateGoogleMaps();
+	}
+
+	//helper method to get suggested airports with input value
+	function getFilteredAirports(key) {
+		var airportObj;
+
+		autoList = [];
+		for(var i = 0; i < usAirports.length; i++) {
+			airportObj = usAirports[i];
+			if(airportObj.code.toLowerCase().indexOf(key) !== -1 || airportObj.name.toLowerCase().indexOf(key) !== -1 || airportObj.city.toLowerCase().indexOf(key) !== -1 || airportObj.state.toLowerCase().indexOf(key) !== -1) {
+				autoList.push(airportObj);
+			}
+			if(autoList.length === 5) {
+				return autoList;
+			}
+		}
+		return autoList;
+	}
+
+	//helper method to construct the airports suggestion list
+	function getSuggestionListItems() {
+		var fragment = document.createDocumentFragment();
+		for(var i = 0; i < autoList.length; i++ ) {
+			var li = document.createElement('li');
+			li.id = autoList[i].code;
+			li.textContent = autoList[i].code + ' ' + autoList[i].city+ ' ' +autoList[i].state;
+			fragment.appendChild(li);
+		}
+		return fragment;
+	}
+
+	//Updating the google maps with two airport points
+	function updateGoogleMaps() {
+		var map,
+			marker,
+			i,
+			markers = [],
+			bounds = new google.maps.LatLngBounds(),
+			infowindow = new google.maps.InfoWindow(),
+			mapContainer = mainContainer.querySelector('#MapContainer'),
+			mapOptions = {
+    	    	mapTypeId: 'roadmap'
+    		};
+
+		markers.push(fromAirportObj);
+		markers.push(toAirportObj);
+
+		map = new google.maps.Map(mapContainer, mapOptions);
+    	map.setTilt(45);
+
+	    for( i = 0; i < markers.length; i++ ) {
+	        var position = new google.maps.LatLng(parseFloat(markers[i].lat), parseFloat(markers[i].lon));
+	        bounds.extend(position);
+	        marker = new google.maps.Marker({
+	            position: position,
+	            map: map,
+	            title: markers[i].name
+	        });
+	          
+	        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+	            return function() {
+	                infoWindow.setContent(markers[i].name);
+	                infoWindow.open(map, marker);
+	            }
+	        })(marker, i));
+
+	        map.fitBounds(bounds);
+	    }
+	}
+	
+	//Requests and Common Functions
+
+	//common request handler method
+	function makeRequest(url, cb) {
+		var xhr = new XMLHttpRequest();
+		xhr.open("GET", url, true);
+
+		xhr.onload = function (e) {
+	        if (xhr.readyState === 4) {
+	            if (xhr.status === 200) {
+		            var res = xhr.responseText;
+	                cb(JSON.parse(res));
+	            } else {
+	                cb(null);
+	            }
+	        }
+	    };
+    	xhr.send(null);
+	}
+
+	//distance finder with two latitude and longitude points
+	function distance(lat1, lon1, lat2, lon2, unit) {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
+
 })();
 
 //Google Map Initialization
